9d251365e5ccd7aae3fac51015d8a3e79b043b57
Agam Dua <agam.dua@contentflow.in>
2014-02-24 22:57:02 +0530

    Added: Call to mock data on home page

--- partial/home/home.js
+++ partial/home/home.js
@@ -3,7 +3,9 @@ angular.module('sourceanno').controller('HomeCtrl',function($scope){
 
     $scope.repos = [ 
         { name: 'repo1', comments:105, url:'url1'}, 
-        { name: 'repo2', comments:15, url:'url2'}
+        { name: 'repo2', comments:15, url:'url2'},
         { name: 'repo3', comments:150, url:'url3'},
         { name: 'repo4', comments:172, url:'url3'}
+    ];
+
 });

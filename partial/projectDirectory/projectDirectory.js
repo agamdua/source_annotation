1c09c3602a8bbb05ce78a0606ab0680e7d777263
Agam Dua <agamdua@gmail.com>
2014-03-06 21:29:54 +0530

    Added: Fetching public repo details from Github
    Also displaying all files located in the project root

--- partial/projectDirectory/projectDirectory.js
+++ partial/projectDirectory/projectDirectory.js
@@ -1,6 +1,11 @@
-angular.module("sourceanno").controller( 'ProjectDirectoryCtrl', function ($scope, $location, Repo) {
-    $scope.message = "We're here";
-    $scope.urlname = $location.url();
-    $scope.folders = Repo.get();
-
+angular.module("sourceanno").controller('ProjectDirectoryCtrl', function ($scope, $location, $http, Repo) {
+	$scope.message = "We're here";
+	$scope.data = {message:"This is some data"};
+	$scope.urlname = $location.url();
+    // $scope.folders = Repo.get();
+    $scope.items = [];
+    $http.get('https://api.github.com/repos/agamdua/StarkMail/contents')
+    .success (function (data) {
+		$scope.items = data; // TODO: Look at interceptors to strip out unnecceary content
+    });
 });
